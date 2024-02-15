22046734cee2c2098cf9c3cfcc7322117a14d938
Arthur Rogado <arthurrogadoreis@hotmail.com>
2024-02-15 11:16:53 -0300

    Refactor code for improved performance and readability

--- database.sql
+++ database.sql
@@ -7,24 +7,6 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-CREATE DATABASE IF NOT EXISTS `fluxocaixa` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci */;
-USE `fluxocaixa`;
-
-CREATE TABLE IF NOT EXISTS `acoes_sistema` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `controlador` varchar(50) NOT NULL,
-  `metodo` varchar(255) NOT NULL,
-  `descricao` varchar(50) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `metodo` (`metodo`),
-  KEY `FK_acoes_sistema_controladores` (`controlador`),
-  CONSTRAINT `FK_acoes_sistema_controladores` FOREIGN KEY (`controlador`) REFERENCES `controladores` (`nome`) ON DELETE NO ACTION ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
-
-INSERT INTO `acoes_sistema` (`id`, `controlador`, `metodo`, `descricao`) VALUES
-	(1, 'CaixasController', 'abrirCaixa', 'Fazer a abertura de um novo caixa.'),
-	(2, 'CaixasController', 'listarCaixas', 'Pegar todos os caixas abertos no escritório do usu');
-
 CREATE TABLE IF NOT EXISTS `caixas` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `nome` varchar(50) NOT NULL,
@@ -41,10 +23,9 @@ CREATE TABLE IF NOT EXISTS `caixas` (
   CONSTRAINT `FK_caixas_escritorios` FOREIGN KEY (`id_escritorio`) REFERENCES `escritorios` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `FK_caixas_usuarios` FOREIGN KEY (`id_usuario_abertura`) REFERENCES `usuarios` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `FK_caixas_usuarios_2` FOREIGN KEY (`id_usuario_fechamento`) REFERENCES `usuarios` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=33 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=40 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 INSERT INTO `caixas` (`id`, `nome`, `observacoes`, `id_escritorio`, `id_usuario_abertura`, `id_usuario_fechamento`, `data_abertura`, `data_fechamento`) VALUES
-	(25, 'Caixa principal', '- Deletar aquele boleto lá! \r\n- Tomar nota do aluno tal. ', 2, 34, NULL, '2024-01-18 15:27:46', NULL),
 	(30, 'Materiais', '', 2, 34, NULL, '2024-01-23 00:25:49', NULL),
 	(32, 'Teste primeiro caixa', 'teste', 3, 33, NULL, '2024-02-05 16:54:24', NULL);
 
@@ -81,7 +62,12 @@ CREATE TABLE IF NOT EXISTS `controladores` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 INSERT INTO `controladores` (`nome`, `exibicao`) VALUES
-	('CaixasController', 'Caixas');
+	('CaixasController', 'Caixas'),
+	('CarteirasController', 'Carteiras'),
+	('EscritoriosController', NULL),
+	('OperacoesController', NULL),
+	('PermissaoController', NULL),
+	('UsuariosController', NULL);
 
 CREATE TABLE IF NOT EXISTS `escritorios` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -97,6 +83,29 @@ INSERT INTO `escritorios` (`id`, `nome`, `cnpj`, `observacoes`, `senha`) VALUES
 	(1, '[ADMIN OFFICE]', '11111111111111', 'Escritório do admin master blaster', ''),
 	(2, 'CFC Jaguar', '09524929000194', 'Autoescola Jaguar de Uruaçu. A melhor!', '$2y$10$Mkwbpy93Udy6BONH5YlvhOzlKyUkrdS3XnzDsbVg85mUBV1i5ovBC'),
 	(3, 'CFC Jaguariúna 2', '45839641000172', 'A melhor autoescola do sul de Goiás!', '$2y$10$DQJN5F7WXwTi3OqT8jBh.eru1Pvg/vWb7R1GsQr8IJa7qK8E1BsqG');
+
+CREATE TABLE IF NOT EXISTS `metodos_sistema` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `titulo` varchar(50) DEFAULT NULL,
+  `metodo` varchar(255) NOT NULL,
+  `controlador` varchar(50) NOT NULL,
+  `descricao` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `metodo` (`metodo`),
+  KEY `FK_acoes_sistema_controladores` (`controlador`),
+  CONSTRAINT `FK_acoes_sistema_controladores` FOREIGN KEY (`controlador`) REFERENCES `controladores` (`nome`) ON DELETE NO ACTION ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+INSERT INTO `metodos_sistema` (`id`, `titulo`, `metodo`, `controlador`, `descricao`) VALUES
+	(1, NULL, 'abrirCaixa', 'CaixasController', 'Fazer a abertura de um novo caixa.'),
+	(2, NULL, 'listarCaixas', 'CaixasController', 'Pegar todos os caixas abertos no escritório do usuário'),
+	(3, NULL, 'editarCaixa', 'CaixasController', NULL),
+	(4, NULL, 'visualizarCaixa', 'CaixasController', NULL),
+	(5, NULL, 'excluirCaixa', 'CaixasController', NULL),
+	(11, 'Ver as operações do caixa', 'getOperacoesCaixa', 'OperacoesController', 'O usuário vai poder visualizar as operações dentro do caixa, assim como os totais.'),
+	(12, NULL, 'criarOperacao', 'OperacoesController', NULL),
+	(13, NULL, 'editarOperacao', 'OperacoesController', NULL),
+	(14, NULL, 'excluirOperacao', 'OperacoesController', NULL);
 
 CREATE TABLE IF NOT EXISTS `operacoes` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -117,27 +126,27 @@ CREATE TABLE IF NOT EXISTS `operacoes` (
   CONSTRAINT `FK__usuarios` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id`) ON DELETE NO ACTION,
   CONSTRAINT `FK_operacoes_caixas` FOREIGN KEY (`id_caixa`) REFERENCES `caixas` (`id`) ON DELETE SET NULL ON UPDATE NO ACTION,
   CONSTRAINT `FK_operacoes_carteira` FOREIGN KEY (`id_carteira`) REFERENCES `carteiras` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=30 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=31 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 INSERT INTO `operacoes` (`id`, `nome`, `observacoes`, `valor`, `id_caixa`, `id_usuario`, `data`, `data_criacao`, `tipo`, `id_carteira`, `excluido`) VALUES
-	(1, 'Curso teórico', '', 502, 25, 34, '2024-01-19', '2024-01-22 22:35:51', 'e', 2, '0'),
-	(2, 'Abastecimento moto', 'Lá no posto tal', 30, 25, 34, '2024-01-22', '2024-01-22 22:51:20', 's', 1, '0'),
-	(4, '', '', 0, 25, 34, '2024-01-23', '2024-01-22 23:00:17', 'e', 3, '1'),
-	(5, 'teste', '', 12, 25, 34, '2024-01-23', '2024-01-22 23:14:17', 'e', 3, '1'),
+	(1, 'Curso teórico', '', 502, NULL, 34, '2024-01-19', '2024-01-22 22:35:51', 'e', 2, '0'),
+	(2, 'Abastecimento moto', 'Lá no posto tal', 30, NULL, 34, '2024-01-22', '2024-01-22 22:51:20', 's', 1, '0'),
+	(4, '', '', 0, NULL, 34, '2024-01-23', '2024-01-22 23:00:17', 'e', 3, '1'),
+	(5, 'teste', '', 12, NULL, 34, '2024-01-23', '2024-01-22 23:14:17', 'e', 3, '1'),
 	(6, 'KIT Completo', 'José dos Anzóis', 50, 30, 34, '2024-01-23', '2024-01-23 00:26:38', 'e', 1, '0'),
 	(7, 'Abastecimento moto', '', 30, 30, 34, '2024-01-23', '2024-01-23 00:27:14', 's', 1, '0'),
 	(8, 'Livrinho', 'Da dona Maria', 20, 30, 34, '2024-01-22', '2024-01-23 00:27:55', 'e', 2, '0'),
-	(9, 'Teste', '', 100, 25, 34, '2024-01-23', '2024-01-23 00:41:09', 'e', 1, '1'),
-	(10, 'teste', '', 80, 25, 34, '2024-01-23', '2024-01-23 01:33:44', 'e', 1, '1'),
-	(11, '', '', 0, 25, 34, '2024-01-23', '2024-01-23 02:03:13', 's', 4, '1'),
-	(12, 'teste', '', 123, 25, 34, '2024-01-23', '2024-01-23 02:03:40', 's', 1, '1'),
-	(13, '', '', 0, 25, 34, '2024-01-23', '2024-01-23 02:06:28', 's', 2, '1'),
-	(14, '', '', 0, 25, 34, '2024-01-23', '2024-01-23 02:06:37', 'e', 4, '1'),
-	(15, '', '', 0, 25, 34, '2024-01-23', '2024-01-23 11:49:11', 'e', 30, '1'),
-	(16, 'teste', '', 123, 25, 34, '2024-01-29', '2024-01-29 17:49:46', 'e', 1, '1'),
-	(17, 'teste', '123123123123', 10, 25, 33, '2024-02-02', '2024-02-05 16:11:27', 'e', 1, '1'),
-	(18, 'teste', '', 12, 25, 33, '2024-02-05', '2024-02-05 16:26:24', 'e', 1, '1'),
-	(19, 'arthur', '', 12, 25, 34, '2024-02-05', '2024-02-05 16:26:59', 'e', 1, '1'),
+	(9, 'Teste', '', 100, NULL, 34, '2024-01-23', '2024-01-23 00:41:09', 'e', 1, '1'),
+	(10, 'teste', '', 80, NULL, 34, '2024-01-23', '2024-01-23 01:33:44', 'e', 1, '1'),
+	(11, '', '', 0, NULL, 34, '2024-01-23', '2024-01-23 02:03:13', 's', 4, '1'),
+	(12, 'teste', '', 123, NULL, 34, '2024-01-23', '2024-01-23 02:03:40', 's', 1, '1'),
+	(13, '', '', 0, NULL, 34, '2024-01-23', '2024-01-23 02:06:28', 's', 2, '1'),
+	(14, '', '', 0, NULL, 34, '2024-01-23', '2024-01-23 02:06:37', 'e', 4, '1'),
+	(15, '', '', 0, NULL, 34, '2024-01-23', '2024-01-23 11:49:11', 'e', 30, '1'),
+	(16, 'teste', '', 123, NULL, 34, '2024-01-29', '2024-01-29 17:49:46', 'e', 1, '1'),
+	(17, 'teste', '123123123123', 10, NULL, 33, '2024-02-02', '2024-02-05 16:11:27', 'e', 1, '1'),
+	(18, 'teste', '', 12, NULL, 33, '2024-02-05', '2024-02-05 16:26:24', 'e', 1, '1'),
+	(19, 'arthur', '', 12, NULL, 34, '2024-02-05', '2024-02-05 16:26:59', 'e', 1, '1'),
 	(20, 'teste', '', 2, NULL, 34, '2024-02-05', '2024-02-05 16:54:08', 'e', 1, '0'),
 	(21, 'jaguariuna2', '0', 1, 32, 34, '2024-02-05', '2024-02-05 17:20:27', 'e', 2, '0'),
 	(22, 'teste', '', 12, 32, 34, '2024-02-11', '2024-02-11 00:19:34', 's', 1, '0'),
@@ -147,21 +156,27 @@ INSERT INTO `operacoes` (`id`, `nome`, `observacoes`, `valor`, `id_caixa`, `id_u
 	(26, 'teste', '', 1, 32, 34, '2024-02-11', '2024-02-11 19:08:02', 'e', 1, '0'),
 	(27, '212asdfas', 'asdf ', 1231, 32, 34, '2024-02-12', '2024-02-12 00:09:58', 'e', 1, '1'),
 	(28, 'Grande', '', 12, 32, 34, '2024-02-12', '2024-02-12 00:15:18', 's', 1, '0'),
-	(29, 'teste cc', '', 11, 32, 34, '2024-02-12', '2024-02-12 00:22:55', 'e', 3, '0');
+	(29, 'teste cc', '', 11, 32, 34, '2024-02-12', '2024-02-12 00:22:55', 'e', 3, '0'),
+	(30, 'parcela 1/5', 'ele vei om', 517, 30, 33, '2024-02-13', '2024-02-14 20:37:24', 'e', 1, '0');
 
 CREATE TABLE IF NOT EXISTS `permissoes` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
   `id_usuario` int(11) NOT NULL,
-  `acao` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`),
+  `id_metodo` int(11) NOT NULL,
   KEY `FK_permissoes_usuarios` (`id_usuario`),
-  KEY `FK_permissoes_acoes_sistema` (`acao`),
-  CONSTRAINT `FK_permissoes_acoes_sistema` FOREIGN KEY (`acao`) REFERENCES `acoes_sistema` (`metodo`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  KEY `FK_permissoes_acoes_sistema` (`id_metodo`),
+  CONSTRAINT `FK_permissoes_acoes_sistema` FOREIGN KEY (`id_metodo`) REFERENCES `metodos_sistema` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `FK_permissoes_usuarios` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
-INSERT INTO `permissoes` (`id`, `id_usuario`, `acao`) VALUES
-	(2, 34, 'listarCaixas');
+INSERT INTO `permissoes` (`id_usuario`, `id_metodo`) VALUES
+	(34, 1),
+	(34, 2),
+	(34, 3),
+	(34, 4),
+	(34, 11),
+	(33, 2),
+	(33, 4),
+	(33, 11);
 
 CREATE TABLE IF NOT EXISTS `usuarios` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
